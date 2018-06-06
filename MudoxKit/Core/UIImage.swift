c0d41ea46859d42897bec6488296a45ac5bdb033
mudox <imudox@gmail.com>
2018-06-06 11:49:19 +0800

    Add `UIImage.mdx.color(_:size:)

--- MudoxKit/Core/UIImage.swift
+++ MudoxKit/Core/UIImage.swift
@@ -1,28 +1,28 @@
 import UIKit
 
-extension UIImage {
+import JacKit
+fileprivate let jack = Jack.with(levelOfThisFile: .verbose)
 
-  
+extension Mudoxive where Base: UIImage {
   /// Generate a image filled with a single color. Typically used for
   /// control background image.
   ///
   /// - Parameters:
   ///   - color: Color to fill.
   ///   - size: Defaults to 1x1 pixel
-  public convenience init(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+  public static func color(_ color :UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
     
     UIGraphicsBeginImageContextWithOptions(size, false, 1) // `size` is pixels
     defer { UIGraphicsEndImageContext() }
-
+    
     color.setFill()
     UIRectFill(CGRect(origin: .zero, size: size))
-
+    
     guard let image = UIGraphicsGetImageFromCurrentImageContext(), let aCGImage = image.cgImage else {
-      self.init()
-      return
+      jack.failure("failed, return a empty image")
+      return UIImage()
     }
     
-    self.init(cgImage: aCGImage)
+    return UIImage(cgImage: aCGImage)
   }
-  
 }
