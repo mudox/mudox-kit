0625bcf6279577d4f37ade5cdf59bdd0b9b8b912
mudox <imudox@gmail.com>
2018-06-08 17:55:28 +0800

    Add `Activity`

---
+++ MudoxKit/Core/Activity.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+
+import JacKit
+fileprivate let jack = Jack.usingLocalFileScope().setLevel(.verbose)
+
+fileprivate var _allIdentifiers: Set<String> = []
+
+public struct Activity {
+
+  /// String identifier
+  public let identifier: String
+
+  /// Maximum count of instance allowed to run at the same time.
+  /// Default: 1
+  public let maxConcurrency: Int
+
+  /// Log out each events of this kind of tasks.
+  /// Used for debugging.
+  /// Default: false
+  public let isLoggingEnbaled: Bool
+
+  /// Keep UIApplication.shared.isNetworkActivityIndicatorVisible to true
+  /// when this kind of tasks are executing.
+  /// Default: false
+  public let isNetworkActivity: Bool
+
+  public init(
+    identifier: String,
+    isNetworkActivity: Bool = false,
+    maxConcurrency: Int = 1,
+    isLoggingEnbaled: Bool = false
+  )
+  {
+    self.maxConcurrency = maxConcurrency
+    self.isLoggingEnbaled = isLoggingEnbaled
+    self.isNetworkActivity = isNetworkActivity
+
+    if !_allIdentifiers.insert(identifier).inserted {
+      Jack.failure("""
+        activity identifier "\(identifier)" has already been used, double check to \
+        use a unique identifier"
+        """)
+    }
+
+    self.identifier = identifier
+  }
+
+}
+
+// MARK: - Hashable
+extension Activity: Hashable {
+
+  public static func == (lhs: Activity, rhs: Activity) -> Bool {
+    return lhs.identifier == rhs.identifier
+  }
+
+  public var hashValue: Int {
+    return identifier.hashValue
+  }
+
+}
+
+// MARK: - Report Events
+extension Activity {
+  public func start() {
+    ActivityCenter.shared.addEvent(.start(self))
+  }
+  
+  public func next(_ element: Any?) {
+    ActivityCenter.shared.addEvent(.next(self, element: element))
+  }
+  
+  public func succeed() {
+    ActivityCenter.shared.addEvent(.succeed(self))
+  }
+  
+  public func fail(_ error: Error) {
+    ActivityCenter.shared.addEvent(.fail(self, error: error))
+  }
+  
+  public func end() {
+    ActivityCenter.shared.addEvent(.end(self))
+  }
+}
