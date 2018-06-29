4641323bd070afd95cc60ac01aa8803d023ec862
mudox <imudox@gmail.com>
2018-06-29 14:30:50 +0800

    Clean up and rename `Errors.swift` -> `CommonError.swift`

--- MudoxKit/Core/CommonError.swift
+++ MudoxKit/Core/CommonError.swift
@@ -1,11 +1,6 @@
-import Foundation
-
-import JacKit
-fileprivate let jack = Jack()
-
 public enum CommonError: Error {
   case cancelled
   case casting(Any?, to: Any.Type)
-  case weakReference
+  case weakRetain
   case error(String)
 }
