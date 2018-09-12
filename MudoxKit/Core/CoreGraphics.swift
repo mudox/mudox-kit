94732eef1c60a6d16cf5c66cb201087b799007c6
mudox <imudox@gmail.com>
2018-09-12 21:45:36 +0800

    Improve CGPoint, CGSize, CGRect extensions

--- MudoxKit/Core/CoreGraphics.swift
+++ MudoxKit/Core/CoreGraphics.swift
@@ -1,32 +1,78 @@
 
 import Foundation
 
+extension CGPoint {
+
+  /// A `CGPoint` with equal x and y value.
+  ///
+  ///     view.frame = CGRect(origin: .of(10), size: .of(80)
+  ///
+  static func of(_ value: CGFloat) -> CGPoint {
+    return CGPoint(x: value, y: value)
+  }
+
+  /// A `CGPoint` with specified x and y value.
+  ///
+  ///     view.frame = CGRect(origin: .of(10, -30), size: .of(80, 120)
+  ///
+  static func of(_ x: CGFloat, _ y: CGFloat) -> CGPoint {
+    return CGPoint(x: x, y: y)
+  }
+
+}
+
 extension CGSize {
-  
-  func aspectFit(to size: CGSize) -> CGSize {
+
+  /// A `CGSize` with equal width and height.
+  ///
+  ///     view.frame = CGRect(origin: .zero, size: .of(80)
+  ///
+  static func of(_ size: CGFloat) -> CGSize {
+    return CGSize(width: size, height: size)
+  }
+
+  /// A `CGSize` with specified width and height.
+  ///
+  ///     view.frame = CGRect(origin: .zero, size: .of(80, 120)
+  ///
+  static func of(_ width: CGFloat, _ height: CGFloat) -> CGSize {
+    return CGSize(width: width, height: height)
+  }
+
+  func aspectFit(into size: CGSize) -> CGSize {
     let wFactor = size.width / width
     let hFactor = size.height / height
     let factor = min(wFactor, hFactor)
     return CGSize(width: width * factor, height: height * factor)
   }
 
-  func aspectFill(to size: CGSize) -> CGSize {
+  func aspectFill(into size: CGSize) -> CGSize {
     let wFactor = size.width / width
     let hFactor = size.height / height
     let factor = max(wFactor, hFactor)
     return CGSize(width: width * factor, height: height * factor)
   }
-  
+
 }
 
 extension CGRect {
-  
-  public static func square(size: CGFloat) -> CGRect {
-    return CGRect(origin: .zero, size: CGSize(width: size, height: size))
+
+  /// A `CGRect` with `.zero` origin and equal width and height.
+  ///
+  ///     view.frame = .of(10.5)
+  ///
+  /// - Returns: A sqaure with `.zero` origin.
+  public static func of(_ size: CGFloat) -> CGRect {
+    return CGRect(origin: .zero, size: .of(size))
   }
-  
-  public static func box(width: CGFloat, height: CGFloat) -> CGRect {
-    return CGRect(origin: .zero, size: CGSize(width: width, height: height))
+
+  /// A `CGRect` with `.zero` origin and specified width and height.
+  ///
+  ///     view.frame = .of(230, 20)
+  ///
+  /// - Returns: A box with `.zero` origin.
+  public static func of(_ width: CGFloat, _ height: CGFloat) -> CGRect {
+    return CGRect(origin: .zero, size: .of(width, height))
   }
-  
+
 }
