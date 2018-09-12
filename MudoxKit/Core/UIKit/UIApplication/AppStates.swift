6cddcb429fbd9f8555bf83b543bd05847c177ce2
mudox <imudox@gmail.com>
2018-09-12 21:49:06 +0800

    Improve `UIApplication.mdx`

--- MudoxKit/Core/UIKit/UIApplication/AppStates.swift
+++ MudoxKit/Core/UIKit/UIApplication/AppStates.swift
@@ -3,18 +3,17 @@ import UIKit
 import JacKit
 fileprivate let jack = Jack()
 
-
 let _appStates: [Notification.Name: String] = [
   // launching state
   .UIApplicationDidFinishLaunching: "App did finish launching",
 
   // active state
   .UIApplicationDidBecomeActive: "App did become active",
-    .UIApplicationWillResignActive: "App will resign active",
+  .UIApplicationWillResignActive: "App will resign active",
 
   // background state
   .UIApplicationDidEnterBackground: "App did enter background",
-    .UIApplicationWillEnterForeground: "App will enter foreground",
+  .UIApplicationWillEnterForeground: "App will enter foreground",
 
   // terminat state
   .UIApplicationWillTerminate: "App will terminate",
@@ -27,14 +26,14 @@ let _appStates: [Notification.Name: String] = [
 
   // status bar
   .UIApplicationWillChangeStatusBarOrientation: "App will change status bar orientation",
-    .UIApplicationDidChangeStatusBarOrientation: "App did change status bar orientation",
-    .UIApplicationWillChangeStatusBarFrame: "App will change status bar frame",
-    .UIApplicationDidChangeStatusBarFrame: "App did change status bar frame",
+  .UIApplicationDidChangeStatusBarOrientation: "App did change status bar orientation",
+  .UIApplicationWillChangeStatusBarFrame: "App will change status bar frame",
+  .UIApplicationDidChangeStatusBarFrame: "App did change status bar frame",
 
   // background refresh
   .UIApplicationBackgroundRefreshStatusDidChange: "App did change background refresh status",
 
   // protected data
   .UIApplicationProtectedDataWillBecomeUnavailable: "App prototected data will become unavailable",
-    .UIApplicationProtectedDataDidBecomeAvailable: "App protected data did become available",
+  .UIApplicationProtectedDataDidBecomeAvailable: "App protected data did become available",
 ]
--- MudoxKit/Core/UIKit/UIApplication/UIApplication+Mdx.swift
+++ MudoxKit/Core/UIKit/UIApplication/UIApplication+Mdx.swift
@@ -3,45 +3,63 @@ import UIKit
 import JacKit
 fileprivate let jack = Jack()
 
-extension Mudoxive where Base: UIApplication {
+var _appStateTrackingTokens: [NSObjectProtocol] = []
 
-  public func openSettingsApp() {
+extension Mudoxive where Base: UIApplication {
+  
+  /// Switch to app' own page in the Settings.app
+  public func openSettingsPage() {
+    let url = URL(string: UIApplicationOpenSettingsURLString)!
     if #available(iOS 10.0, *) {
-      UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!)
+      UIApplication.shared.open(url)
     } else {
-      UIApplication.shared.openURL(URL(string: UIApplicationOpenSettingsURLString)!)
+      UIApplication.shared.openURL(url)
     }
   }
+  
+  public func setupAtLaunchTime() {
+    dumpBasicInfo()
+    startTrackingStateChanges()
+  }
 
-  /**
-   Log a line when App state transition happens.
-   */
-  public func tracksAppStates() {
-    _appStates.forEach { item in
+  /// Log a line when App state transition happens.
+  public func startTrackingStateChanges() {
+    if !_appStateTrackingTokens.isEmpty {
+      stopTrackingStateChanges()
+    }
+    
+    _appStateTrackingTokens = _appStates.map { item in
       The.notificationCenter.addObserver(forName: item.key, object: nil, queue: nil, using: { _ in
-        Jack(Info.appName).debug(item.value, options: [.noLocation])
+        Jack().debug("⭐️ \(item.value)", options: .messageOnly)
       })
     }
   }
+  
+  public func stopTrackingStateChanges() {
+    if _appStateTrackingTokens.isEmpty {
+      jack.warn("\(#function) is invoked when there is no tokens to remove")
+    }
+    _appStateTrackingTokens.forEach(The.notificationCenter.removeObserver)
+  }
 
-  public func dumpInfo() {
+  public func dumpBasicInfo() {
     let lines = """
-      🚀 \(Info.appName)
-      [App]
-        - Name       :   \(Info.appName)
-        - ID         :   \(Info.appBundleID ?? "N/A")
-        - Release    :   \(Info.appRelease ?? "N/A")  (CFBundleShortVersionString)
-        - Build      :   \(Info.appBuild ?? "N/A") (kCFBundleVersionKey)
-        - Debug      :   \(Info.isDebug)
-        - Simulator  :   \(Info.isSimulator)
-      [Device]
-        - Name       :   \(Info.deviceName)
-        - Model      :   \(Info.deviceModel)
-        - UUID       :   \(Info.deviceUUID ?? "N/A")
-      [System]
-        - Name       :   \(Info.systemName)
-        - Version    :   \(Info.systemVersion)
-      """ + "\n"
+    🚀 \(Info.appName)
+    [App]
+      - Name       :   \(Info.appName)
+      - ID         :   \(Info.appBundleID ?? "N/A")
+      - Release    :   \(Info.appRelease ?? "N/A")  (CFBundleShortVersionString)
+      - Build      :   \(Info.appBuild ?? "N/A") (kCFBundleVersionKey)
+      - Debug      :   \(Info.isDebug)
+      - Simulator  :   \(Info.isSimulator)
+    [Device]
+      - Name       :   \(Info.deviceName)
+      - Model      :   \(Info.deviceModel)
+      - UUID       :   \(Info.deviceUUID ?? "N/A")
+    [System]
+      - Name       :   \(Info.systemName)
+      - Version    :   \(Info.systemVersion)
+    """ + "\n"
     Jack(Info.appName).info(lines, options: .messageOnly)
   }
 }
