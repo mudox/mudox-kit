991f858624d873118d6e3d4a3f90381625f51a96
mudox <imudox@gmail.com>
2018-11-02 02:01:01 +0800

    Add new overload of `ViewControllers.create`

--- MudoxKit/Core/UIKit/View Controller/ViewControllers.swift
+++ MudoxKit/Core/UIKit/View Controller/ViewControllers.swift
@@ -34,44 +34,93 @@ public enum ViewControllers {
   ///   - _: Type of the view controller.
   ///   - identifier: Identifier of the view controller in the storyboard, if
   ///     nil, the initial view controller is loaded.
-  ///   - storyboard: The name of the nib file, without any leading path information.
+  ///   - storyboard: The name of the storyboard file, without any leading path information.
   ///   - bundle: The bundle in which to search for the nib file. If you
   ///     specify nil, this method looks for the nib file in the main bundle.
   ///
   /// - Returns: The loaded view controller instance.
-  public static func create<ViewController: UIViewController>(
-    _: ViewController.Type,
+  public static func create<VC: UIViewController>(
+    _: VC.Type,
     identifier: String? = nil,
     storyboard: String,
     bundle: Bundle? = nil
   )
-    -> ViewController
+    -> VC
   {
     let storyboard = UIStoryboard(name: storyboard, bundle: bundle)
     if let id = identifier {
-      return storyboard.instantiateViewController(withIdentifier: id) as! ViewController
+      return storyboard.instantiateViewController(withIdentifier: id) as! VC
     } else {
-      return storyboard.instantiateInitialViewController() as! ViewController
+      return storyboard.instantiateInitialViewController() as! VC
     }
   }
-
-  /// Load view controller from the given nib file.
+  
+  /// Load view controller from given storyboard as `UIViewController`.
+  ///
+  /// Load initial view controller of a storyboard in the main bundle.
+  /// ```
+  /// ViewControllers.create(storyboard: "main")
+  /// ```
+  ///
+  /// Load view controller with given identifier from a storyboard in the main bundle.
+  /// ```
+  /// ViewControllers.create(
+  ///   identifier: "login",
+  ///   storyboard: "main"
+  /// )
+  /// ```
+  ///
+  /// Load view controller with given identifier from a storyboard in a given bundle.
+  /// ```
+  /// let bundle = Bundle(for: SomeClass.self)
+  /// ViewControllers.create(
+  ///   identifier: "login",
+  ///   storyboard: "main",
+  ///   bundle: bundle
+  /// )
+  /// ```
   ///
   /// - Parameters:
   ///   - _: Type of the view controller.
-  ///   - nibName: File name of nib file.
+  ///   - identifier: Identifier of the view controller in the storyboard, if
+  ///     nil, the initial view controller is loaded.
+  ///   - storyboard: The name of the storyboard file, without any leading path information.
   ///   - bundle: The bundle in which to search for the nib file. If you
   ///     specify nil, this method looks for the nib file in the main bundle.
   ///
   /// - Returns: The loaded view controller instance.
-  public static func create<ViewController: UIViewController>(
-    _: ViewController.Type,
-    nibName: String,
+  public static func create(
+    identifier: String? = nil,
+    storyboard: String,
+    bundle: Bundle? = nil
+    )
+    -> UIViewController
+  {
+    let storyboard = UIStoryboard(name: storyboard, bundle: bundle)
+    if let id = identifier {
+      return storyboard.instantiateViewController(withIdentifier: id)
+    } else {
+      return storyboard.instantiateInitialViewController()!
+    }
+  }
+  /// Load view controller from the given nib file.
+  ///
+  /// - Parameters:
+  ///   - _: Type of the view controller.
+  ///   - nib: File name of nib file.
+  ///   - bundle: The bundle in which to search for the nib file. If you
+  ///     specify nil, this method looks for the nib file in the main bundle.
+  ///
+  /// - Returns: The loaded view controller instance.
+  public static func create<VC: UIViewController>(
+    _: VC.Type,
+    nib: String,
     bundle: Bundle? = nil
   )
-    -> ViewController?
+    -> VC
   {
-    let nib = UINib(nibName: nibName, bundle: bundle)
-    return nib.instantiate(withOwner: nil, options: [:]).first as? ViewController
+    let nib = UINib(nibName: nib, bundle: bundle)
+    return nib.instantiate(withOwner: nil, options: [:]).first as! VC
   }
+  
 }
