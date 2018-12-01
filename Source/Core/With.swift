909883aa9acbdedb383aa3bd75aacaeaaa897e37
mudox <imudox@gmail.com>
2018-12-02 06:00:09 +0800

    Remove `With.swift` use `Then` library instead

--- Source/Core/With.swift
+++
@@ -1,7 +0,0 @@
-public func with<T: AnyObject>(_ target: T, apply: (T) throws -> ()) rethrows {
-  try apply(target)
-}
-
-//public func with<T: AnyObject, Result>(_ target: T, evaluate: (T) throws -> Result) rethrows -> Result {
-//  return try evaluate(target)
-//}
